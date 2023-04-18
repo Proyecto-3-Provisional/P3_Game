67265a2234ae6892a439ef6d51969dac1b206d5b
PabloEtayo <paetayo@ucm.es>
2023-04-18 09:39:39 +0200

    Proyecto creado
    Tipo dll, vacío con una función de prueba

---
+++ TheRiseOfSuxalote/TheRiseOfSuxalote/Game.h
@@ -0,0 +1,13 @@
+#ifdef SUXALOTE_EXPORTS
+#define SUXALOTE_API __declspec(dllexport)
+#else
+#define SUXALOTE_API __declspec(dllimport)
+#endif
+
+class SUXALOTE_API Game
+{
+public:
+	Game() {}
+	void prueba();
+};
+
