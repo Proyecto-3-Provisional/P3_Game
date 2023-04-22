c9186ca2f3f7195c4a6a90b311ecc9f5108f45e9
David Brey <dbrey@ucm.es>
2023-04-22 12:31:50 +0200

    Plantilla PlayerController
    Co-Authored-By: PabloEtayo <62880904+PabloEtayo@users.noreply.github.com>

--- sources/TheRiseOfSuxalote/Game.cpp
+++ sources/TheRiseOfSuxalote/Game.cpp
@@ -1,6 +1,10 @@
 #include "Game.h"
 #include <iostream>
 
+#include <EC/entity.h>
+#include <Physics/rigidbody.h>
+
+
 Game::Game()
 {
 
@@ -8,4 +12,42 @@ Game::Game()
 
 void prueba() {
 	std::cout << "Funciona el juego\n";
+
+	// Crear un componente nuevo 
+	// Llegar al inputmanager, para detectar input
+	// Acceder al rigidbody, para meter AddForce
+
+	
+	
 }
+
+namespace magma_game
+{
+	PlayerController::PlayerController()
+	{
+
+	}
+
+	PlayerController::~PlayerController()
+	{
+
+	}
+
+	bool PlayerController::initComponent()
+	{
+
+		return false;
+	}
+
+	void PlayerController::update(float deltaTime)
+	{
+		//Singleton<magma_engine::InputManager>::instance()->showOrHideMouse();
+		
+		rb = ent->getComponent<magma_engine::Rigidbody>();
+		rb->isEnable();
+		//rb->addForce(magma_engine::Vector3D(0,0,0));
+		//rb->isOnCollision();
+		//rb->isCollideWith(0);
+	}
+}
+
--- sources/TheRiseOfSuxalote/Game.h
+++ sources/TheRiseOfSuxalote/Game.h
@@ -4,8 +4,31 @@
 #define SUXALOTE_API __declspec(dllimport)
 #endif
 
+#include <EC/component.h>
+
+class Rigidbody;
+
 class SUXALOTE_API Game {
 public:
 	Game();
 };
 extern "C" SUXALOTE_API void prueba();
+
+namespace magma_game
+{
+	class PlayerController : public magma_engine::ec::Component
+	{
+	public:
+		PlayerController();
+		~PlayerController();
+
+		bool initComponent() override;
+		void update(float deltaTime) override;
+
+
+	private:
+		magma_engine::Rigidbody* rb;
+		
+	};
+}
+
